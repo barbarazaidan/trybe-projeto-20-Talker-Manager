afd0308d2cec401bf84aa86ad00810f2b11fe3c8
Bárbara <babivaldez@gmail.com>
2023-07-06 16:18:47 -0300

    ajusta o arquivo connection e cria o arquivo .env

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,12 +1,21 @@
 const mysql = require('mysql2/promise');
 
 // o createPool se torna responsável por gerenciar a abertura e fechamento das conexões.
+// const connection = mysql.createPool({
+//   // pode ser o nome do container (neste caso, talker_manager_db) ou o nome do serviço
+//   host: 'db',
+//   port: 3306,
+//   user: 'root',
+//   password: 'password',
+//   database: 'TalkerDB',
+// });
+
 const connection = mysql.createPool({
-  host: 'db', // pode ser o nome do container (neste caso, talker_manager_db) ou o nome do serviço
-  port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'TalkerDB',
+  host: process.env.MYSQL_HOSTNAME,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = connection;
